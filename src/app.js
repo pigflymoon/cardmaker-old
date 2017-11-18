0a427405a4b6462ba82190b1c6d26704da7139d5
pigflymoon <pplflymoon2015@gmail.com>
2017-11-19 07:31:33 +1300

    iconloader

--- src/app.js
+++ src/app.js
@@ -1,24 +1,10 @@
 import {Navigation} from 'react-native-navigation';
-import Icon from 'react-native-vector-icons/Ionicons';
 
 import {registerScreens} from './screens';
+import {prepareIcons} from './utils/IconsLoader';
 
 registerScreens(); // this is where you register all of your app's screens
 
-// start the app
-
-
-async function prepareIcons() {
-    const icons = await Promise.all([
-        Icon.getImageSource('ios-home', 30),
-        Icon.getImageSource('ios-settings', 30),
-        Icon.getImageSource('ios-bookmarks', 30),
-        Icon.getImageSource('ios-heart', 30),
-        Icon.getImageSource('ios-person', 30),
-    ]);
-    const [home, settings, cards, notifications, profile] = icons;
-    return {home, settings, cards, notifications, profile};
-}
 
 // and then
 async function startApp() {
