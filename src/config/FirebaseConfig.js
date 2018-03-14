4a0aaf75f3de372fffad561656443bb2b4d1be35
pigflymoon <pplflymoon2015@gmail.com>
2018-03-15 12:36:21 +1300

    firebase config

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -7,15 +7,6 @@ var fireBaseconfig = {
     storageBucket: "cardmaker-31ae8.appspot.com",
     messagingSenderId: "409995152460"
 };
-//dev
-// var fireBaseconfig = {
-//     apiKey: "AIzaSyBHdEif0MF0SHVW-IuuIq1NXdW2dKWc_1A",
-//     authDomain: "cardmaker-bca47.firebaseapp.com",
-//     databaseURL: "https://cardmaker-bca47.firebaseio.com",
-//     projectId: "cardmaker-bca47",
-//     storageBucket: "cardmaker-bca47.appspot.com",
-//     messagingSenderId: "149962974009"
-// };
 var firebaseApp = firebase.initializeApp(fireBaseconfig);
 const db = firebaseApp.database();
 const auth = firebaseApp.auth();
