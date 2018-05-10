87ceb0e9b5824698213de2b9cce2f8d910815792
pigflymoon <pplflymoon2015@gmail.com>
2018-05-10 16:13:17 +1200

    unlock modal slide--to be continued

--- src/screens/settings/UnLockModal.js
+++ src/screens/settings/UnLockModal.js
@@ -4,15 +4,116 @@ import {
     View,
     Button,
 } from 'react-native';
+import Carousel, { Pagination } from 'react-native-snap-carousel';
+import SliderEntry from '../../components/SliderEntry';
+
+import {sliderWidth, itemWidth} from '../../styles/sliderEntry';
+import unlockModalStyle from '../../styles/unlockModal';
+import colors from '../../styles/colors';
+const SLIDER_1_FIRST_ITEM = 1;
+
+export const ENTRIES1 = [
+    {
+        title: 'Beautiful and dramatic Antelope Canyon',
+        subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+        illustration: 'https://i.imgur.com/UYiroysl.jpg'
+    },
+    {
+        title: 'Earlier this morning, NYC',
+        subtitle: 'Lorem ipsum dolor sit amet',
+        illustration: 'https://i.imgur.com/UPrs1EWl.jpg'
+    },
+    {
+        title: 'White Pocket Sunset',
+        subtitle: 'Lorem ipsum dolor sit amet et nuncat ',
+        illustration: 'https://i.imgur.com/MABUbpDl.jpg'
+    },
+    {
+        title: 'Acrocorinth, Greece',
+        subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+        illustration: 'https://i.imgur.com/KZsmUi2l.jpg'
+    },
+    {
+        title: 'The lone tree, majestic landscape of New Zealand',
+        subtitle: 'Lorem ipsum dolor sit amet',
+        illustration: 'https://i.imgur.com/2nCt3Sbl.jpg'
+    },
+    {
+        title: 'Middle Earth, Germany',
+        subtitle: 'Lorem ipsum dolor sit amet',
+        illustration: 'https://i.imgur.com/lceHsT6l.jpg'
+    }
+];
 export  default class UnLockModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            slider1ActiveSlide: SLIDER_1_FIRST_ITEM
+        };
+    }
+
+    _renderItemWithParallax({item, index}, parallaxProps) {
+        return (
+            <SliderEntry
+                data={item}
+                even={(index + 1) % 2 === 0}
+                parallax={true}
+                parallaxProps={parallaxProps}
+            />
+        );
+    }
+
+    renderSlide = () => {
+        const {slider1ActiveSlide} = this.state;
+
+        return (
+            <View style={unlockModalStyle.exampleContainer}>
+                <Text style={unlockModalStyle.title}>{`Example`}</Text>
+                <Carousel
+                    ref={c => this._slider1Ref = c}
+                    data={ENTRIES1}
+                    renderItem={this._renderItemWithParallax}
+                    sliderWidth={sliderWidth}
+                    itemWidth={itemWidth}
+                    hasParallaxImages={true}
+                    firstItem={SLIDER_1_FIRST_ITEM}
+                    inactiveSlideScale={0.94}
+                    inactiveSlideOpacity={0.7}
+                    // inactiveSlideShift={20}
+                    containerCustomStyle={unlockModalStyle.slider}
+                    contentContainerCustomStyle={unlockModalStyle.sliderContentContainer}
+                    loop={true}
+                    loopClonesPerSide={2}
+                    autoplay={true}
+                    autoplayDelay={500}
+                    autoplayInterval={3000}
+                    onSnapToItem={(index) => this.setState({slider1ActiveSlide: index}) }
+                />
+                <Pagination
+                    dotsLength={ENTRIES1.length}
+                    activeDotIndex={slider1ActiveSlide}
+                    containerStyle={unlockModalStyle.paginationContainer}
+                    dotColor={'rgba(255, 255, 255, 0.92)'}
+                    dotStyle={unlockModalStyle.paginationDot}
+                    inactiveDotColor={colors.black}
+                    inactiveDotOpacity={0.4}
+                    inactiveDotScale={0.6}
+                    carouselRef={this._slider1Ref}
+                    tappableDots={!!this._slider1Ref}
+                />
+            </View>
+        )
+    }
+
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text style={{ fontSize: 30 }}>This is a modal!</Text>
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                <Text style={{fontSize: 30}}>This is a modal!</Text>
                 <Button
                     onPress={() => this.props.navigation.goBack()}
                     title="Dismiss"
                 />
+                {this.renderSlide()}
             </View>
         );
     }
---
+++ src/styles/unlockModal.js
@@ -0,0 +1,49 @@
+import { StyleSheet, Dimensions, Platform } from 'react-native';
+import colors from '../styles/colors';
+
+const IS_IOS = Platform.OS === 'ios';
+const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
+
+
+
+
+export default{
+    exampleContainer: {
+        paddingVertical: 30
+    },
+
+    title: {
+        paddingHorizontal: 30,
+        backgroundColor: 'transparent',
+        color: 'rgba(255, 255, 255, 0.9)',
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    subtitle: {
+        marginTop: 5,
+        paddingHorizontal: 30,
+        backgroundColor: 'transparent',
+        color: 'rgba(255, 255, 255, 0.75)',
+        fontSize: 13,
+        fontStyle: 'italic',
+        textAlign: 'center'
+    },
+    slider: {
+        marginTop: 15,
+        overflow: 'visible' // for custom animations
+    },
+    sliderContentContainer: {
+        paddingVertical: 10 // for custom animation
+    },
+    paginationContainer: {
+        paddingVertical: 8
+    },
+    paginationDot: {
+        width: 8,
+        height: 8,
+        borderRadius: 4,
+        marginHorizontal: 8
+    }
+}
+
