f36bc3559a06682cc2715d653f23a0191f6001ab
pigflymoon <pplflymoon2015@gmail.com>
2018-05-29 14:00:38 +1200

    category config

---
+++ src/config/CategoryConfig.js
@@ -0,0 +1,16 @@
+const cardsType = {
+    holiday: ["christmas", "newYear", "easter"],
+    birthday: ["kids", "forHer", "forHim"],
+    thankyou: ["general", "birthday", "wedding"]
+}
+
+const invitationsType = {
+    holiday: ["christmas", "newYear", "easter"],
+    birthday: ["kids", "women", "men"],
+    wedding: ["invitation", "saveTheDate", "rsvp"]
+}
+
+export default CategoryConfig = {
+    cards: cardsType,
+    invitations: invitationsType
+}
