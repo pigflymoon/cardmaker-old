5df5ba8c889bbb0626e4680b7c3d85a9ec5ff7a1
pigflymoon <pplflymoon2015@gmail.com>
2018-08-28 16:32:58 +1200

    update version number

--- src/screens/settings/Settings.js
+++ src/screens/settings/Settings.js
@@ -40,7 +40,7 @@ export default class Settings extends Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
-            version: '2.1.1',
+            version: '2.1.4',
             isPro: 'Disabled',
             versionColor: colors.grey2,
             bgImage: graybg,
