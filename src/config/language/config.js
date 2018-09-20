eda295180fb38e612dec060316de63e495dc516f
pigflymoon <pplflymoon2015@gmail.com>
2018-09-20 14:57:19 +1200

    language config

---
+++ src/config/language/config.js
@@ -0,0 +1,26 @@
+export default LanguageConfig = {
+    lan: [{
+        value: 'English(English)'
+    }, {
+        value: '简体中文(Chinese-simplified)'
+    }, {
+        value: '繁體中文(Chinese-traditional)'
+    },{
+        value:'日本語(Japanese)'
+    },{
+        value:'Español(Spanish)'
+    },{
+        value:'Français(French)'
+    },{
+        value:'Deutsch(German)'
+    },{
+        value:'Português(Portuguese)'
+    },{
+        value:'한국어(Korean)'
+    },
+    //     {
+    //     value:'العربية(Arabic)'
+    // }
+    
+        ]
+}
