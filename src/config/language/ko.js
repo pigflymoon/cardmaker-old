2c1b91d5c20965b3bc247d9968a298f9fc586b16
pigflymoon <pplflymoon2015@gmail.com>
2018-09-24 20:59:41 +1200

    language config

--- src/config/language/ko.js
+++ src/config/language/ko.js
@@ -13,23 +13,23 @@ export default {
 
     changeToSystem:'change to System',
     tabs:{
-        exploreTranslation:'Explore',
-        makeCardTranslation:'Make Card',
-        profileTranslation:'Profile',
-        settingsTranslation:'Settings'
+        exploreTranslation:'홈',
+        makeCardTranslation:'카드 만들기',
+        profileTranslation:'윤곽',
+        settingsTranslation:'설정'
     },
     screens:{
-        exploreTranslation:'',
-        imagesGalleryTranslation:'',
-        cardsDeckTranslation:'',
-        myFavoritesTranslation:'',
-        makeCardTranslation:'',
-        makeInvitationTranslation:'',
-        welcomeTranslation:'',
-        confirmEmailTranslation:'',
-        resetPasswordTranslation:'',
-        termsTranslation:'',
-        policyTranslation:'',
+        exploreTranslation:'홈',
+        imagesGalleryTranslation:'이미지 갤러리',
+        cardsDeckTranslation:'카드 덱',
+        myFavoritesTranslation:'내가 좋아하는 것들',
+        makeCardTranslation:'카드 만들기',
+        makeInvitationTranslation:'초대장 만들기',
+        welcomeTranslation:'환영',
+        confirmEmailTranslation:'이메일 확인',
+        resetPasswordTranslation:'암호를 재설정',
+        termsTranslation:'이용약관',
+        policyTranslation:'개인정보처리방침',
         aboutTranslation:'',
     },
     exploreTab:{
