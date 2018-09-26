20453d6b9b872bb925ad7d3317c9cb1e5600acf1
pigflymoon <pplflymoon2015@gmail.com>
2018-09-27 10:48:34 +1200

    es

--- src/config/language/es.js
+++ src/config/language/es.js
@@ -45,6 +45,56 @@ export default {
         titleTranslation:'Bienvenido a la aplicación Cardmaker',
         descriptionTranslation:'Por favor, elija su imagen de la biblioteca para hacer su tarjeta, ¡diviértase!',
         signoutTransplation:'Cerrar sesión'
+    },
+    cards: {
+        holiday: {
+            title: 'la fiesta',
+            types: [{"christmas": "la Navidad"}, {"newYear": "Año nuevo"}, {"easter": "Pascua de Resurrección"}],
+        },
+        birthday: {
+            title: 'cumpleaños',
+            types: [{"kids": "niños"}, {"forHer": "para ella"}, {"forHim": "para él"}]
+        },
+        thankyou: {
+            title: 'gracias',
+            types: [{"general": "general"}, {"birthday": "cumpleaños"}, {"wedding": "Boda"}]
+        },
+        occasions: {
+            title: 'ocasiones',
+            types: [{"anniversary": "aniversario"}, {"goodLuck": "buena suerte"}, {"teacherAppreciation": "Aprecio del maestro"}]
+        },
+        congratulations: {
+            title: 'Felicidades',
+            types: [{"graduation": "Graduación"}, {"newBaby": "nuevo bebé"}, {"exam": "examen"}]
+        },
+        thoughtsFeelings: {
+            title: 'pensamientos y sentimientos',
+            types: [{"cheerUp": "alegrarse"}, {"friendship": "amistad"}, {"getWell": "Mejórate"}, {"loveRomance": "amor y romance"}]
+        }
+    },
+    invitations: {
+        holiday: {
+            title: 'la fiesta',
+            types: [{"christmas": "la Navidad"}, {"newYear": "Año nuevo"}, {"easter": "Pascua de Resurrección"}]
+        },
+        birthday: {
+            title: 'cumpleaños',
+            types: [{"kids": "niños"}, {"women": "mujer"}, {"men": "hombres"}],
+
+        },
+        wedding: {
+            title: 'Boda',
+            types: [{"invitation": "invitación"}, {"saveTheDate": "Reserva la fecha"}, {"rsvp": "rsvp"}],
+        },
+        party: {
+            title: 'fiesta',
+            types: [{"anniversary": "aniversario"}, {"graduationParty": "fiesta de graduación"}, {"bbqParty": "fiesta de barbacoa"}],
+        },
+        announcement: {
+            title: 'la comunicación',
+            types: [{"birth": "nacimiento"}, {"graduationAnnouncement": "Anuncio de graduación"}],
+        },
     }
+
 
 }
