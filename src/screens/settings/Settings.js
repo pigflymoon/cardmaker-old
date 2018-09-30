0e39f634d39d611c615826659aebb22bfeea6646
pigflymoon <pplflymoon2015@gmail.com>
2018-09-30 13:02:26 +1300

    clean

--- src/screens/settings/Settings.js
+++ src/screens/settings/Settings.js
@@ -51,7 +51,7 @@ export default class Settings extends Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
-            version: '2.1.4',
+            version: '2.1.5',
             isPro: 'Disabled',
             versionColor: colors.grey2,
             bgImage: graybg,
